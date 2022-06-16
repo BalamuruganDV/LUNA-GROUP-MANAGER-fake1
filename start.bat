6837c72941688c9a5688e94b7fe48439d7c5cf6a
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-06-16 16:43:36 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE MR JOKER BOT
+TITLE LUNA-GROUP-MANAGER
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m mrjoker
+env\scripts\activate.bat && py -m luna-group-manager
